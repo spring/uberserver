56f2d205ae41b469f3d1f238d9143025148543ef
lunixbochs <lunixbochs@gmail.com>
2008-11-17 22:18:43 -0800

    unbroke merge

--- SQLUsers.py
+++ SQLUsers.py
@@ -1,6 +1,7 @@
 import time
 
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Boolean
+from sqlalchemy.exceptions import OperationalError
 from sqlalchemy.orm import mapper, sessionmaker, relation
 
 #for user in session.query(User).filter(User.name=='ed'):
@@ -29,69 +30,82 @@ from sqlalchemy.orm import mapper, sessionmaker, relation
 metadata = MetaData()
 
 class User(object):
-	def __init__(self, name, password, last_ip):
+	def __init__(self, name, casename, password, last_ip, access='user'):
 		self.name = name
+		self.casename = casename
 		self.password = password
 		self.last_login = int(time.time())
-		self.banned = 0
+		self.register_date = int(time.time())
 		self.last_ip = last_ip
 		self.ingame_time = 0
 		self.bot = 0
-		self.access = 'user' # moderator, admin
+		self.access = access # user, moderator, admin
 
 	def __repr__(self):
 		return "<User('%s', '%s')>" % (self.name, self.password)
 
 class Address(object):
 	def __init__(self, ip_address):
-		self.time = int(time.time())
+		self.first_time = int(time.time())
+		self.last_time = int(time.time())
+		self.count = 1
 		self.ip_address = ip_address
 
 	def __repr__(self):
 		return "<Address('%s')>" % self.ip_address
 
-#class Channel(object):
-#	def __init__(self, name, owner='', topic='', topic_time=0, topic_owner='', antispam='', admins='', autokick='ban', censor=False, antishock=False):
-#		self.name = name
-#		self.password = password
-#		self.owner = owner
-#		self.topic = topic
-#		self.topic_time = topic_time
-#		self.topic_owner = topic_owner
-#		self.antispam = antispam
-#		self.admins = admins
-#		self.allow = allow
-#		self.autokick = autokick
-#		self.censor = censor
-#		self.antishock = antishock
+class Rename(object):
+	def __init__(self, original, new):
+		self.original = original
+		self.new = new
+		self.time = int(time.time())
+		
+	def __repr__(self):
+		return "<Rename('%s')>" % self.ip_address
 
-#	def __repr__(self):
-#		return "<Channel('%s')>" % self.name
+class Channel(object):
+	def __init__(self, name, password='', chanserv=False, owner='', topic='', topic_time=0, topic_owner='', antispam='', admins='', autokick='ban', censor=False, antishock=False):
+		self.name = name
+		self.password = password
+		self.chanserv = chanserv
+		self.owner = owner
+		self.topic = topic
+		self.topic_time = topic_time
+		self.topic_owner = topic_owner
+		self.antispam = antispam
+		self.admins = admins
+		self.allow = allow
+		self.autokick = autokick
+		self.censor = censor
+		self.antishock = antishock
 
-#class ChanUser(object):
-#	def __init__(self, name, channel, admin=False, banned='', allowed=False, mute=0):
-#		self.name = name
-#		self.channel = channel
-#		self.admin = admin
-#		self.banned = banned
-#		self.allowed = allowed
-#		self.mute = mute
+	def __repr__(self):
+		return "<Channel('%s')>" % self.name
 
-#	def __repr__(self):
-#		return "<ChanUser('%s')>" % self.name
+class ChanUser(object):
+	def __init__(self, name, channel, admin=False, banned='', allowed=False, mute=0):
+		self.name = name
+		self.channel = channel
+		self.admin = admin
+		self.banned = banned
+		self.allowed = allowed
+		self.mute = mute
 
-#class Antispam(object):
-#	def __init__(self, enabled, quiet, duration, timeout, bonus, unique, bonuslength):
-#		self.enabled = enabled
-#		self.quiet = quiet
-#		self.duration = duration
-#		self.timeout = timeout
-#		self.bonus = bonus
-#		self.unique = unique
-#		self.bonuslength = bonuslength
+	def __repr__(self):
+		return "<ChanUser('%s')>" % self.name
 
-#	def __repr__(self):
-#		return "<Antispam('%s')>" % self.channel
+class Antispam(object):
+	def __init__(self, enabled, quiet, duration, timeout, bonus, unique, bonuslength):
+		self.enabled = enabled
+		self.quiet = quiet
+		self.duration = duration
+		self.timeout = timeout
+		self.bonus = bonus
+		self.unique = unique
+		self.bonuslength = bonuslength
+
+	def __repr__(self):
+		return "<Antispam('%s')>" % self.channel
 
 class Ban(object):
 	def __init__(self, reason, end_time):
@@ -112,10 +126,12 @@ class AggregateBan(object):
 users_table = Table('users', metadata,
 	Column('id', Integer, primary_key=True),
 	Column('name', String(40)),
+	Column('casename', String(40)),
 	Column('password', String(32)),
+	Column('register_date', Integer),
 	Column('last_login', Integer), # use seconds since unix epoch
 	Column('last_ip', String(15)), # would need update for ipv6
-	Column('ingame', Integer),
+	Column('ingame_time', Integer),
 	Column('access', String(32)),
 	Column('bot', Integer),
 	)
@@ -123,44 +139,54 @@ users_table = Table('users', metadata,
 addresses_table = Table('addresses', metadata, 
 	Column('id', Integer, primary_key=True),
 	Column('ip_address', String(15), nullable=False),
-	Column('time', Integer),
+	Column('first_time', Integer),
+	Column('last_time', Integer),
+	Column('count', Integer),
 	Column('user_id', Integer, ForeignKey('users.id')),
 	)
 
-#channels_table = Table('channels', metadata,
-#	Column('id', Integer, primary_key=True),
-#	Column('name', String(40)),
-#	Column('password', String(32)),
-#	Column('owner', String(40)),
-#	Column('topic', Integer),
-#	Column('topic_time', Integer),
-#	Column('topic_owner', String(40)),
-#	Column('antispam', ForeignKey('antispam.id')),
-#	Column('autokick', String(5)),
-#	Column('censor', Boolean),
-#	Column('antishock', Boolean),
-#	)
+renames_table = Table('renames', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('user_id', Integer, ForeignKey('users.id')),
+	Column('original', String(40)),
+	Column('new', String(40)),
+	Column('time', Integer),
+	)
 
-#chanuser_table = Table('chanuser', metadata,
-#	Column('id', Integer, primary_key=True),
-#	Column('name', String(40)),
-#	Column('channel', String(40)),
-#	Column('admin', Boolean),
-#	Column('banned', String),
-#	Column('allowed', Boolean),
-#	Column('mute', Integer),
-#	)
+channels_table = Table('channels', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('name', String(40)),
+	Column('password', String(32)),
+	Column('owner', String(40)),
+	Column('topic', Integer),
+	Column('topic_time', Integer),
+	Column('topic_owner', String(40)),
+	Column('antispam_id', Integer, ForeignKey('antispam.id')),
+	Column('autokick', String(5)),
+	Column('censor', Boolean),
+	Column('antishock', Boolean),
+	)
 
-#antispam_table = Table('antispam', metadata,
-#	Column('id', Integer, primary_key=True),
-#	Column('enabled', Boolean),
-#	Column('quiet', Boolean),
-#	Column('duration', Integer),
-#	Column('timeout', Integer),
-#	Column('bonus', Integer),
-#	Column('unique', Integer),
-#	Column('bonuslength', Integer),
-#	)
+chanuser_table = Table('chanuser', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('name', String(40)),
+	Column('channel', String(40)),
+	Column('admin', Boolean),
+	Column('banned', String(120)),
+	Column('allowed', Boolean),
+	Column('mute', Integer),
+	)
+
+antispam_table = Table('antispam', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('enabled', Boolean),
+	Column('quiet', Boolean),
+	Column('duration', Integer),
+	Column('timeout', Integer),
+	Column('bonus', Integer),
+	Column('unique', Integer),
+	Column('bonuslength', Integer),
+	)
 
 bans_table = Table('ban_groups', metadata, # server bans
 	Column('id', Integer, primary_key=True),
@@ -172,18 +198,20 @@ aggregatebans_table = Table('ban_items', metadata, # server bans
 	Column('id', Integer, primary_key=True),
 	Column('type', String(10)), # what exactly is banned (user, ip, subnet, hostname, ip range, etc)
 	Column('data', String(60)), # regex would be cool
-	Column('ban_id', ForeignKey('bans.id')),
+	Column('ban_id', Integer, ForeignKey('ban_groups.id')),
 	)
 
-mapper(Address, addresses_table)
 mapper(User, users_table, properties={
-	'addresses':relation(Address, backref='user', cascade="all, delete, delete-orphan")
+	'addresses':relation(Address, backref='user', cascade="all, delete, delete-orphan"),
+	'renames':relation(Rename, backref='user', cascade="all, delete, delete-orphan"),
 	})
-#mapper(Channel, channels_table, properties={
-	'antispam':relation(AntiSpam, backref='channel', cascade="all, delete, delete-orphan")
+mapper(Address, addresses_table)
+mapper(Rename, renames_table)
+mapper(Channel, channels_table, properties={
+	'antispam':relation(Antispam, backref='channel', cascade="all, delete, delete-orphan"),
 	})
-#mapper(ChanUser, chanuser_table)
-#mapper(AntiSpam, antispam_table)
+mapper(ChanUser, chanuser_table)
+mapper(Antispam, antispam_table)
 mapper(Ban, bans_table)
 mapper(AggregateBan, aggregatebans_table, properties={
 	'ban':relation(Ban, backref='item', cascade="all, delete, delete-orphan")
@@ -192,7 +220,8 @@ mapper(AggregateBan, aggregatebans_table, properties={
 #metadata.create_all(engine)
 
 class UsersHandler:
-	def __init__(self, engine):
+	def __init__(self, root, engine):
+		self._root = root
 		metadata.create_all(engine)
 		Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
 		self.session = Session()
@@ -201,55 +230,84 @@ class UsersHandler:
 		results = self.session.query(AggregateBan)
 		subnetbans = results.filter(AggregateBan.type=='subnet')
 		userban = results.filter(AggregateBan.type=='user').filter(AggregateBan.data==user)
-		ipban = results.filter(AggregateBan.type=='ip').filter(AggregateBan.data=ip)
+		ipban = results.filter(AggregateBan.type=='ip').filter(AggregateBan.data==ip)
 		useridban = results.filter(AggregateBan.type=='userid').filter(AggregateBan.data==userid)
 		
-
 	def login_user(self, user, password, ip, userid=None):
+		name = user.lower()
+		lanadmin = self._root.lanadmin
+		if user == lanadmin['username'] and password == lanadmin['password']:
+			sqluser = User(name, lanadmin['username'], password, ip, 'admin')
+			return True, sqluser
 		good = True
 		now = int(time.time())
-		entry = self.session.query(User).filter(User.name==user).first() # should only ever be one user with each name so we can just grab the first one :)
+		entry = self.session.query(User).filter(User.name==name).first() # should only ever be one user with each name so we can just grab the first one :)
 		reason = entry
 		if not entry:
 			return False, 'No user named %s'%user
 		if not password == entry.password:
 			good = False
 			reason = 'Invalid password'
-		if entry.banned > 0:
-			if entry.banned > now:
-				good = False
-				timeleft = entry.banned - now
-				daysleft = '%.2f'%(float(seconds) / 60 / 60 / 24)
-				if daysleft >= 1:
-					reason = 'You are banned: (%s) days remaining.' % daysleft
-				else:
-					reason = 'You are banned: (%s) hours remaining.' % (float(seconds) / 60 / 60)
+		#if entry.banned > 0:
+			#if entry.banned > now:
+				#good = False
+				#timeleft = entry.banned - now
+				#daysleft = '%.2f'%(float(seconds) / 60 / 60 / 24)
+				#if daysleft >= 1:
+					#reason = 'You are banned: (%s) days remaining.' % daysleft
+				#else:
+					#reason = 'You are banned: (%s) hours remaining.' % (float(seconds) / 60 / 60)
 		exists = self.session.query(Address).filter(Address.user_id==entry.id).first()
-		if not exists:
-			entry.addresses.append(Address(ip_address=ip))
+		if not exists: entry.addresses.append(Address(ip_address=ip))
+		else:
+			exists.last_time = now
+			exists.count += 1
 		entry.last_login = now
 		entry.last_ip = ip
 		self.session.commit()
 		return good, reason
 
 	def register_user(self, user, password, ip): # need to add better ban checks so it can check if an ip address is banned when registering an account :)
-		results = self.session.query(User).filter(User.name==user).first()
+		if self._root.censor:
+			if not self._root.SayHooks._nasty_word_censor(user):
+				return False, 'Name failed to pass profanity filter.'
+		name = user.lower()
+		results = self.session.query(User).filter(User.name==name).first()
+		lanadmin = self._root.lanadmin
+		if name == lanadmin['username'].lower():
+			if password == lanadmin['password']: # if you register a lanadmin account with the right user and pass combo, it makes it into a normal admin account
+				if user in self._root.usernames:
+					self._root.usernames[user]
+				entry = User(name, lanadmin['username'], password, ip, 'admin')
+				entry.addresses.append(Address(ip_address=ip))
+				self.session.save(entry)
+				self.session.commit()
+				return True, 'Account registered successfully.'
+			else: return False, 'Username already exists.'
 		if results:
 			return False, 'Username already exists.'
-		entry = User(user, password, ip)
-        	entry.addresses.append(Address(ip_address=ip))
+		entry = User(name, user, password, ip)
+		entry.addresses.append(Address(ip_address=ip))
 		self.session.save(entry)
 		self.session.commit()
 		return True, 'Account registered successfully.'
 
 	def rename_user(self, user, newname):
-		results = self.session.query(User).filter(User.name==newname).first()
+		if self._root.censor:
+			if not self._root.SayHooks._nasty_word_censor(user):
+				return False, 'Name failed to pass profanity filter.'
+		lnewname = newname.lower()
+		results = self.session.query(User).filter(User.name==lnewname).first()
 		if results:
 			return False, 'Username already exists.'
-		entry = self.session.query(User).filter(User.name==user).first()
-		entry.name = newname
-		self.session.save(entry)
+		entry = self.session.query(User).filter(User.name==user.lower()).first()
+		if not entry: return False, 'You don\'t seem to exist anymore. Contact an admin or moderator.'
+		entry.name = lnewname
+		entry.casename = newname
+		#self.session.save(entry)
 		self.session.commit()
+		# need to iterate through channels and rename junk there...
+		# it might actually be a lot easier to use userids in the server...
 		return True, 'Account renamed successfully.'
 
 	def remove_user(self, user):
@@ -260,25 +318,34 @@ class UsersHandler:
 		self.session.commit()
 		return True, 'Success.'
 	
-#	def load_channels(self):
-#		response = self.session.query(Channel)
-#		for channel in response:
-#			channels.append({})
-#		return channels
+	def load_channels(self):
+		response = self.session.query(Channel)
+		for channel in response:
+			channels.append({})
+		return channels
 	
-#	def save_channel(self, channel):
-#		entry = self.session.query(Channel)
-#		entry.password = channel[
-#		entry.owner = channel[
-#		entry.topic = channel[
-#		entry.topic_time = channel[
-#		entry.topic_owner = channel[
-#		entry.antispam = channel[
-#		entry.autokick = channel[
-#		entry.censor = channel[
-#		entry.antishock = channel[
-#		entry.censor = channel[
+	def save_channel(self, channel):
+		entry = self.session.query(Channel)
+		entry.password = channel['password']
+		entry.chanserv = channel['chanserv']
+		entry.owner = channel['owner']
+		topic = channel['topic']
+		if topic:
+			topic = topic['text']
+			topic_time = topic['time']
+			topic_owner = topic['owner']
+		else:
+			topic, topic_time, topic_owner = ('', 0, '')
+		entry.topic = topic
+		entry.topic_time = topic_time
+		entry.topic_owner = topic_owner
+		#entry.antispam = channel[]
+		entry.autokick = channel['autokick']
+		entry.censor = channel['censor']
+		entry.antishock = channel['antishock']
+		self.session.save(entry)
 
-#	def save_channels(self, channels):
-#		for channel in channels:
-#			self.save_channel(channel)
+	def save_channels(self, channels):
+		for channel in channels:
+			self.save_channel(channel)
+		self.session.commit()
