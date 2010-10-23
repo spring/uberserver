11d4254c893fe52ae28eff9b75cd2e1ef75ea2fc
lunixbochs <lunixbochs@gmail.com>
2010-10-23 16:18:47 -0600

    fixed the py2exe build script

--- scripts/build_exe.py
+++ scripts/build_exe.py
@@ -1,3 +1,13 @@
+# if this is running from the scripts folder, move up a folder.
+import os, sys
+if not 'server.py' in os.listdir('.') and 'scripts' in os.listdir('..'):
+	os.chdir('..')
+
+sys.path.append('.')
+
+import sys
+sys.argv = [sys.argv[0], 'py2exe']
+
 from distutils.core import setup
 import py2exe
 
@@ -6,21 +16,23 @@ setup(
 	options = {'py2exe': {'compressed': 1,
 							'optimize': 1,
 							'bundle_files': 1,
-							'excludes': ['_ssl',
-										'fetch_deps',
-										],
-							'includes': ['ChanServ',
+							'excludes': [
+								'_ssl',
+							],
+							'includes': [
+								'ChanServ',
 								'Client',
-								'ClientHandler',
 								'DataHandler',
-								'ip2country',
+								'Dispatcher',
 								'LANUsers',
 								'Multiplexer',
 								'NATServer',
 								'Protocol',
 								'SayHooks',
 								'SQLUsers',
-								'Telnet'],
+								'Telnet',
+								'tasserver',
+							],
 						}},
 	zipfile = None,
 	ascii = True,
