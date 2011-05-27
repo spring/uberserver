31778949cf631aea46f287880e07f4ee2a3ad886
lunixbochs <lunixbochs@gmail.com>
2011-05-26 22:37:44 -0600

    fix issue #1 (whatismyip.com api change)

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ _root.console_write('Detecting online IP:')
 try:
 	timeout = socket.getdefaulttimeout()
 	socket.setdefaulttimeout(5)
-	web_addr = urlopen('http://whatismyip.com/automation/n09230945.asp').read()
+	web_addr = urlopen('http://automation.whatismyip.com/n09230945.asp').read()
 	socket.setdefaulttimeout(timeout)
 	_root.console_write(web_addr)
 except:
