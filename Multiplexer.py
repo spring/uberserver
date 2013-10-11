2dfd39dade7a50977bb39f5eff1506c651257e0b
abma <spring@abma.de>
2013-10-11 14:41:21 +0200

    remove unused multiplexer & try to fix #24 try 2

--- Multiplexer.py
+++ Multiplexer.py
@@ -44,34 +44,6 @@ class BaseMultiplexer:
 	def pollUnregister(self, fd): pass
 	def pollSetoutput(self, fd, ready): pass
 
-class SelectMultiplexer(BaseMultiplexer):
-	def poll(self):
-		if not self.sockets: return ([], [] ,[])
-		try: return select(self.sockets, self.output, [], 0.1)
-		except error:
-			inputs = []
-			outputs = []
-			errors = []
-			for s in self.sockets:
-				try: select([s], [s], [], 0.01)
-				except:
-					errors.append(s)
-					self.unregister(s)
-			inputs, outputs, _ = select(self.sockets, self.output, [], 0.1)
-			return inputs, outputs, errors
-
-class KqueueMultiplexer(BaseMultiplexer):
-	def __init__(self):
-		raise NotImplementedError
-		self.poller = kqueue()
-		self.socketmap = {}
-		self.__multiplex_init__()
-		
-	def pollRegister(self, fd): pass
-	def pollUnregister(self, fd): pass
-	def pollSetoutput(self, fd, ready): pass
-
-
 class BasePollMultiplexer(BaseMultiplexer):
 	
 	def __init__(self):
@@ -103,7 +75,7 @@ class BasePollMultiplexer(BaseMultiplexer):
 		if not fd in self.socketToFileno: return
 		eventmask = self.inMask | self.errMask | (self.outMask if ready else 0)
 		self.poller.modify(fd, eventmask) # not valid for select.poll before python 2.6, might need to replace with register() in this context
-		
+
 	def poll(self):
 		for i in xrange(5):
 			try:
@@ -125,9 +97,10 @@ class BasePollMultiplexer(BaseMultiplexer):
 		outMask = self.outMask
 		errMask = self.errMask
 		for fd, mask in results:
-			if mask & inMask: inputs.append(self.filenoToSocket[fd])
-			if mask & outMask: outputs.append(self.filenoToSocket[fd])
-			if mask & errMask: errors.append(self.filenoToSocket[fd])
+			s = self.filenoToSocket[fd]
+			if mask & inMask: inputs.append(s)
+			if mask & outMask: outputs.append(s)
+			if mask & errMask: errors.append(s)
 		return inputs, outputs, errors
 	
 class EpollMultiplexer(BasePollMultiplexer):
@@ -141,22 +114,5 @@ class EpollMultiplexer(BasePollMultiplexer):
 		self.poller = epoll()
 		self.__poll_init__()
 
-class PollMultiplexer(BasePollMultiplexer):
-	def __init__(self):
-		self.inMask = POLLIN | POLLPRI
-		self.outMask = POLLOUT
-		self.errMask = POLLERR | POLLHUP | POLLNVAL
-		
-		self.args = [250]
-		
-		self.poller = poll()
-		self.__poll_init__()
-
-BestMultiplexer = SelectMultiplexer
-if 'kqueue' in dir() and False: # not implemented
-	BestMultiplexer = KqueueMultiplexer
-elif 'epoll' in dir():
-	BestMultiplexer = EpollMultiplexer
-elif 'poll' in dir():
-	BestMultiplexer = PollMultiplexer
+BestMultiplexer = EpollMultiplexer
 
