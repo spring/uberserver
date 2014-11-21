280bfc7a38f4240382fef08638d08e67b999d0dc
rt <rt@rt-0.(none)>
2014-11-21 17:45:12 +0100

    -

--- testclient.py
+++ testclient.py
@@ -232,8 +232,8 @@ class LobbyClient:
 
 		if (arg0 == "ACCEPTED"):
 			server_key_sig = DECODE_FUNC(arg1)
-			client_key_str = SECURE_HASH_FUNC(self.aes_cipher_obj.get_key())
-			client_key_sig = client_key_str.digest()
+			client_key_sha = SECURE_HASH_FUNC(self.aes_cipher_obj.get_key())
+			client_key_sig = client_key_sha.digest()
 
 			print("\tserver_key_sig=%s client_key_sig=%s" % (server_key_sig, client_key_sig))
 
@@ -241,7 +241,7 @@ class LobbyClient:
 			self.valid_shared_key = True
 
 			## now check for data manipulation or corruption
-			can_send_ack_shared_key = (server_key_digest == client_key_digest)
+			can_send_ack_shared_key = (server_key_sig == client_key_sig)
 		else:
 			## rejected as invalid or cleared
 			pass
