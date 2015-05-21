c728431cdb21947236860200d2b2b03e7d19a44b
abma <abma@users.noreply.github.com>
2015-05-21 17:19:33 +0200

    part two of #169, remove compat flag 'eb'

--- protocol/Protocol.py
+++ protocol/Protocol.py
@@ -185,7 +185,6 @@ flag_map = {
 	'b': 'battleAuth',       # JOINBATTLEREQUEST/ACCEPT/DENY
 	'sp': 'scriptPassword',  # scriptPassword in JOINEDBATTLE
 	'et': 'sendEmptyTopic',  # send NOCHANNELTOPIC on join if channel has no topic
-	'eb': 'extendedBattles', # deprecated use cl instead
 	'm': 'matchmaking',      # FORCEJOINBATTLE from battle hosts for matchmaking
 	'cl': 'cleanupBattles',  # BATTLEOPENED / OPENBATTLE with support for engine/version
 	'p':  'agreementPlain',  # AGREEMENT is plaintext
@@ -539,9 +538,6 @@ class Protocol:
 			client.Send("MOTD Your client doesn't support the 'p' compatibility flag, please upgrade it!")
 			client.Send("MOTD see htpp://springrts.com/dl/LobbyProtocol/ProtocolDescription.html#0.37")
 			missing_flags += ' p'
-		if client.compat['eb']:
-			self.out_SERVERMSG(client, "Your client uses the 'eb' compatibility flag, which is replaced by 'cl' please update it!")
-			self.out_SERVERMSG(client, "see http://springrts.com/dl/LobbyProtocol/ProtocolDescription.html#0.37")
 		if len(missing_flags) > 0:
 			self._root.console_write('[%s] <%s> client "%s" missing compat flags:%s'%(client.session_id, client.username, client.lobby_id, missing_flags))
 
