a6614ed8f940c4f20d42abc39b04dd951d5af2c1
abma <spring@abma.de>
2015-05-23 01:16:18 +0200

    cleanup / first working version

--- twistedserver.py
+++ twistedserver.py
@@ -10,11 +10,11 @@ class Chat(LineReceiver, Client.Client):
 
 	def __init__(self, root):
 		self.root = root
-		self.users = {}
 		assert(self.root.protocol.userdb)
 
 	def connectionMade(self):
 		self.root.sessions += 1
+		self.root.clients[self.root.sessions] = self
 		peer = (self.transport.getPeer().host, self.transport.getPeer().port)
 		super(Chat, self).__init__( self.root, None, peer, self.root.sessions)
 		self.root.protocol._new(self)
@@ -28,32 +28,17 @@ class Chat(LineReceiver, Client.Client):
 
 	def dataReceived(self, data):
 		data = data.strip("\n")
-		print "Received: '%s'" %(data)
 		for line in data.split("\n"):
 			if line.strip():
 				self.root.protocol._handle(self, line)
 
 	def Send(self, msg):
-		print "Sending: '%s'" %(msg)
 		self.transport.write(msg.encode("utf-8")+"\n")
 
-	def FlushBuffer(self): #noop
+	def FlushBuffer(self): #Client overrides
 		pass
 
-	def set_msg_id(self, msg):
-		self.msg_id = ""
-
-		if (not msg.startswith('#')):
-			return msg
-
-		test = msg.split(' ')[0][1:]
-
-		if (not test.isdigit()):
-			return msg
-
-		self.msg_id = '#%s ' % test
-		return (' '.join(msg.split(' ')[1:]))
-	def Remove(self, reason): #Client overrides
+	def Remove(self, reason):
 		pass
 
 class ChatFactory(Factory):
