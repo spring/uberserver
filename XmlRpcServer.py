15823462a787d9cc97aa0a3b8b458686a8fe4598
abma <spring@abma.de>
2016-08-11 20:57:36 +0200

    remove Protocol.py dependency from XmlRpcServer.py

--- XmlRpcServer.py
+++ XmlRpcServer.py
@@ -4,7 +4,6 @@
 # xmlrpc class for auth of replays.springrts.com
 #
 # TODO:
-#  - remove dependency to Protocol.py
 #  - move SQLAlchemy calls to SQLUsers.py
 
 import BaseHTTPServer
@@ -13,9 +12,9 @@ from base64 import b64encode
 import os.path
 import logging
 import socket
+import traceback
 from logging.handlers import TimedRotatingFileHandler
 
-from protocol import Protocol
 from SQLUsers import User, Rename, Login
 import SQLUsers
 from sqlalchemy import and_
@@ -39,6 +38,8 @@ sqlurl = 'sqlite:///server.db'
 xmlhost = "localhost"
 xmlport = 8300
 
+engine = sqlalchemy.create_engine(sqlurl)
+userdb = SQLUsers.UsersHandler(None, engine)
 
 def _xmlrpclog(self, format, *args):
 	logger.debug("%s - %s" , self.client_address[0], format%args)
@@ -46,37 +47,46 @@ def _xmlrpclog(self, format, *args):
 # overwrite default logger, because it will otherwise spam main server log
 BaseHTTPServer.BaseHTTPRequestHandler.log_message = _xmlrpclog
 
-class fakeRoot():
-	userdb = None
-	SayHooks = None
-	def __init__(self, sqlurl):
-		engine = sqlalchemy.create_engine(sqlurl)
-		self.userdb = SQLUsers.UsersHandler(self, engine)
-	def getUserDB(self):
-		return self.userdb
-	def clientFromUsername(self, username):
-		return self.userdb.clientFromUsername(username)
-
-proto = Protocol.Protocol(fakeRoot(sqlurl))
-
 class XmlRpcServer(object):
 	"""
 		XMLRPC service, exported functions are in class _RpcFuncs
 	"""
 	def __init__(self, host, port):
-		self.host = host
-		self.port = port
-		self._server = SimpleXMLRPCServer((self.host, self.port))
+		self._server = SimpleXMLRPCServer((host, port))
 		self._server.register_introspection_functions()
 		self._server.register_instance(_RpcFuncs())
-
-	def start(self):
-		logger.info('Listening for XMLRPC clients on %s:%d', self.host, self.port)
+		logger.info('Listening for XMLRPC clients on %s:%d', host, port)
 		self._server.serve_forever()
 
 	def shutdown(self):
 		self._server.shutdown()
 
+
+def validateLogin(username, password):
+	password_enc = unicode(b64encode(LEGACY_HASH_FUNC(password).digest()))
+
+	user = userdb.clientFromUsername(username)
+	if not user:
+		logger.warning("User not found: %s" %(username))
+		return {"status": 1}
+	good = userdb.legacy_test_user_pwrd(user, password_enc)
+	logger.debug("reply: %s", good)
+	if not good:
+		logger.error("Invalid password: %s" %(username))
+		return {"status": 1}
+	session = userdb.sessionmaker() # FIXME: move to SQLUsers.py
+	db_user = session.query(User.id, User.username, User.ingame_time, User.email).filter(User.username == username).first()
+	renames = session.query(Rename.original).distinct(Rename.original).filter(Rename.user_id == db_user.id).all()
+	renames = [ r[0] for r in renames]
+
+	country = session.query(Login.country).filter(and_(Login.country != '??', Login.country != None, Login.country != '')).first()
+	result = {"status": 0, "accountid": int(db_user.id), "username": str(db_user.username),
+			"ingame_time": int(db_user.ingame_time), "email": str(db_user.email),
+			"aliases": renames,
+			"country": country[0] if country else ''
+		 }
+	session.close()
+	return result
 
 class _RpcFuncs(object):
 	"""
@@ -84,30 +94,13 @@ class _RpcFuncs(object):
 	"""
 
 	def get_account_info(self, username, password):
-		password_enc = unicode(b64encode(LEGACY_HASH_FUNC(password).digest()))
-		good = proto._testlogin(unicode(username), password_enc) # FIXME: don't use Protocol.py
-		logger.debug("reply: %s", good)
-		if not good:
+		try:
+			return validateLogin(username, password)
+		except Exception as e:
+			logger.error('Exception: %s: %s' %(str(e), str(traceback.format_exc())))
 			return {"status": 1}
-		session = self._root.userdb.sessionmaker() # FIXME: move to SQLUsers.py
-		db_user = session.query(User.id, User.username, User.ingame_time, User.email).filter(User.username == username).first()
-		renames = session.query(Rename.original).distinct(Rename.original).filter(Rename.user_id == db_user.id).all()
-		renames = [ r[0] for r in renames]
-
-		country = session.query(Login.country).filter(and_(Login.country != '??', Login.country != None, Login.country != '')).first()
-		result = {"status": 0, "accountid": int(db_user.id), "username": str(db_user.username),
-				"ingame_time": int(db_user.ingame_time), "email": str(db_user.email),
-				"aliases": renames,
-				"country": country[0] if country else ''
-			 }
-
-		session.close()
-		return result
-
 try:
         xmlrpcserver = XmlRpcServer(xmlhost, xmlport)
-	xmlrpcserver.start()
-	logger.info('Listening for XMLRPC clients on %s:%d' % (xmlhost, xmlport))
 except socket.error:
         logger.error('Error: Could not start XmlRpcServer.')
 
