f357631c9b7cbbdf256e9ff797ffd3f332cb381e
abma <spring@abma.de>
2017-10-12 03:30:00 +0200

    prefix cmd with DICT to simplify parsing

--- protocol/Protocol.py
+++ protocol/Protocol.py
@@ -2052,7 +2052,7 @@ class Protocol:
 			return
 		msgs = self.userdb.get_channel_messages(client.db_id, channel.id, timestamp)
 		for msg in msgs:
-			client.Send('SAID ' + self._dictToTags({ "chanName": chan, "time": str(datetime_totimestamp(msg[0])), "userName": msg[1], "msg": msg[2]}))
+			client.Send('DICT ' + self._dictToTags({"cmd": "SAID", "chanName": chan, "time": str(datetime_totimestamp(msg[0])), "userName": msg[1], "msg": msg[2]}))
 
 	def in_FORCELEAVECHANNEL(self, client, chan, username, reason=''):
 		'''
