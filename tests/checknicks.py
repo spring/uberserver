dc20a563f8a9940a0049f4e64130c624c2eaef8f
abma <spring@abma.de>
2018-02-25 14:04:18 +0100

    add bad nick test tool

---
+++ tests/checknicks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import SayHooks
+
+if len(sys.argv) < 2:
+	print("Usage: %s nick ..." %(sys.argv[0]))
+	sys.exit(-1)
+tocheck = sys.argv[1]
+
+for check in sys.argv[1:]:
+	for nick in SayHooks.bad_nick_list:
+		if nick in check:
+			print("%s is in nasty word list: %s" %(nick, check))
+
