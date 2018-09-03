b7f2517566120382c9f4a1736609efe9475b7f44
TurBoss <j.l.toledano.l@gmail.com>
2018-09-03 12:32:24 +0200

    change tabs to 4 spaces

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 # coding=utf-8
 
 try:
-	import thread
+    import thread
 except:
-	# thread was renamed to _thread in python 3
-	import _thread
+    # thread was renamed to _thread in python 3
+    import _thread
 
 import traceback, signal, socket, sys, logging
 from twisted.internet import reactor
@@ -17,7 +17,7 @@ from DataHandler import DataHandler
 from Client import Client
 from NATServer import NATServer
 
-import ip2country # just to make sure it's downloaded
+import ip2country  # just to make sure it's downloaded
 import ChanServ
 import twistedserver
 
@@ -25,16 +25,18 @@ _root = DataHandler()
 _root.parseArgv(sys.argv)
 
 try:
-	signal.SIGHUP
+    signal.SIGHUP
 
-	def sighup(sig, frame):
-		logging.info('Received SIGHUP.')
-		if _root.sighup:
-			_root.reload()
 
-	signal.signal(signal.SIGHUP, sighup)
+    def sighup(sig, frame):
+        logging.info('Received SIGHUP.')
+        if _root.sighup:
+            _root.reload()
+
+
+    signal.signal(signal.SIGHUP, sighup)
 except AttributeError:
-	pass
+    pass
 
 logging.info('Starting uberserver...')
 
@@ -42,33 +44,32 @@ natport = _root.natport
 backlog = 100
 
 try:
-	natserver = NATServer(natport)
-	try:
-		thread.start_new_thread(natserver.start,())
-	except NameError:
-		_thread.start_new_thread(natserver.start,())
-	natserver.bind(_root)
+    natserver = NATServer(natport)
+    try:
+        thread.start_new_thread(natserver.start, ())
+    except NameError:
+        _thread.start_new_thread(natserver.start, ())
+    natserver.bind(_root)
 except socket.error:
-	print('Error: Could not start NAT server - hole punching will be unavailable.')
+    print('Error: Could not start NAT server - hole punching will be unavailable.')
 
-logging.info('Using %i client handling thread(s).'%_root.max_threads)
+logging.info('Using %i client handling thread(s).' % _root.max_threads)
 
 _root.init()
 
 try:
-	reactor.listenTCP(_root.port, twistedserver.ChatFactory(_root))
-	print('Started lobby server!')
-	print('Connect the lobby client to')
-	print('  public:  %s:%d' %(_root.online_ip, _root.port))
-	print('  private: %s:%d' %(_root.local_ip, _root.port))
-	reactor.run()
+    reactor.listenTCP(_root.port, twistedserver.ChatFactory(_root))
+    print('Started lobby server!')
+    print('Connect the lobby client to')
+    print('  public:  %s:%d' % (_root.online_ip, _root.port))
+    print('  private: %s:%d' % (_root.local_ip, _root.port))
+    reactor.run()
 
 except KeyboardInterrupt:
-	logging.info()
-	logging.info('Server killed by keyboard interrupt.')
+    logging.info()
+    logging.info('Server killed by keyboard interrupt.')
 except:
-	logging.error(traceback.format_exc())
-	logging.info('Deep error, exiting...')
+    logging.error(traceback.format_exc())
+    logging.info('Deep error, exiting...')
 
 _root.shutdown()
-
