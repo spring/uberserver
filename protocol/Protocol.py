e5874180125e01f7ccb012dfdad83dfcda44216f
silentwings <3885930+silentwings@users.noreply.github.com>
2018-09-05 12:50:59 +0100

    more useful message in CHANGEEMAILREQUESTDENIED

--- protocol/Protocol.py
+++ protocol/Protocol.py
@@ -2999,7 +2999,7 @@ class Protocol:
 			self.out_SERVERMSG(client, "compatibility flag cl needed")
 			return
 		if not self.verificationdb.active():
-			client.Send("CHANGEEMAILREQUESTDENIED email verification is currently turned off, you do not need a verification code!")
+			client.Send("CHANGEEMAILREQUEST email verification is currently turned off, a blank verification code will be accepted!")
 			return
 		newmail = newmail.lower()
 		reason = "requested to change your email address on the SpringRTS lobbyserver (" + client.username + ")"
