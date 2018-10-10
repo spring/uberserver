d5c939863b4a8a505ac8193b3afb6e3364dd2fe8
TurBoss <j.l.toledano.l@gmail.com>
2018-10-10 22:53:07 +0200

    test to python 3.4+

--- tests/XmlRpcTestClient.py
+++ tests/XmlRpcTestClient.py
@@ -2,10 +2,8 @@
 # coding=utf-8
 # This file is part of the uberserver (GPL v2 or later), see LICENSE
 
-import xmlrpclib
+import xmlrpc.client
 
-proxy = xmlrpclib.ServerProxy("http://localhost:8300/")
+proxy = xmlrpc.client.ServerProxy("http://localhost:8300/")
 print(proxy.get_account_info("ubertest01", "KeepItSecretKeepItSafe01"))
 print(proxy.get_account_info("doesn'texist", "nope"))
-
-
