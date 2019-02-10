f5856d0fceea665a482b8359e9d561af4ae1f77c
TurBoss <j.l.toledano.l@gmail.com>
2019-02-10 23:15:14 +0100

    forgotten change

--- XmlRpcServer.py
+++ XmlRpcServer.py
@@ -77,7 +77,7 @@ def validateLogin(username, password):
 		logger.error("User has no access: <%s> %s" %(username, db_user.access))
 		return {"status": 1}
 
-	banned = session.query(Ban.reason).filter(Ban.user_id == db_user.id, datetime.datetime.now() <= Ban.end_time).first()
+	banned = session.query(Ban.reason).filter(Ban.user_id == db_user.id, datetime.datetime.now() <= Ban.end_date).first()
 	if banned:
 		session.close()
 		logger.warning("User is banned: %s" %(username, banned.reason))
