0bd9c287811e30b97a98b768b6107ba2cbdc0ebd
silentwings <3885930+silentwings@users.noreply.github.com>
2019-02-18 16:48:26 +0000

    fix natType>0

--- protocol/Battle.py
+++ protocol/Battle.py
@@ -65,7 +65,7 @@ class Battle(Channel):
 			client.Send('DISABLEUNITS %s' % ' '.join(self.disabled_units))
 
 		if self.natType > 0:
-			if battle.host == client.session_id:
+			if self.host == client.session_id:
 				raise NameError('%s is having an identity crisis' % (client.name))
 			if client.udpport:
 				self._root.usernames[host].Send('CLIENTIPPORT %s %s %s' % (username, client.ip_address, client.udpport))
