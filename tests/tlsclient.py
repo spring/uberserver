5ff6ffe11f4511486d7eec068518b669e128ac87
abma <spring@abma.de>
2020-01-09 21:02:27 +0100

    show used ciphers

--- tests/tlsclient.py
+++ tests/tlsclient.py
@@ -9,14 +9,16 @@ context = ssl.SSLContext()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('lobby.springrts.com', 8200))
 
-print(s.recv(1024))
+print(s.recv(1024).decode())
 s.send(b"STLS\n")
 assert(s.recv(1024).startswith(b"OK"))
 
 s = context.wrap_socket(s)
 
-print(s.recv(1024))
+print(s.recv(1024).decode())
 cert = s.getpeercert(True)
+
+print(s.cipher())
 
 m = hashlib.sha256()
 m.update(cert)
@@ -27,7 +29,7 @@ print("Certificate fingerprint: %s" %(m.hexdigest()))
 assert(m.hexdigest() == "0124dc0f4295b401a2d81ade3dc81b7a467eb9a70b0a4912b5e15fede735fe73")
 
 s.send(b"PING\n")
-print(s.recv(1024))
+print(s.recv(1024).decode())
 
 s.shutdown(socket.SHUT_RDWR)
 s.close()
