976e1131fd1314dbd30b2355db2ad491117ddd50
silentwings <3885930+silentwings@users.noreply.github.com>
2020-09-08 09:18:38 +0100

    GETUSERINFO prints last_Login when online

--- protocol/Protocol.py
+++ protocol/Protocol.py
@@ -2486,19 +2486,19 @@ class Protocol:
 				self.out_SERVERMSG(client, "User '%s' does not exist" % username)
 				return
 			register_date = user.register_date.strftime('%b %d, %Y') if user.register_date else 'unknown'
+			last_login = user.last_login.strftime('%b %d, %Y, %H:%M:%S') if user.last_login else 'unknown'
 			if user.username in self._root.usernames:
 				if user.static:
 					self.out_SERVERMSG(client, "User <%s> is static" % username)
 					return
 				self.out_SERVERMSG(client, "<%s> is online,  user_id=%d, session_id=%d" % (user.username, user.user_id, user.session_id))
-				self.out_SERVERMSG(client, "Agent: %s" % (user.agent))
-				self.out_SERVERMSG(client, "Registered %s" % (register_date))
 				ingame_time = int(self._root.usernames[user.username].ingame_time)	
 			else:
 				self.out_SERVERMSG(client, "<%s> is offline,  user_id=%s" % (user.username, user.user_id))
-				self.out_SERVERMSG(client, "Agent: %s" % (user.last_agent))
-				self.out_SERVERMSG(client, "Registered %s,  last login %s" % (register_date, user.last_login.strftime('%b %d, %Y')))
 				ingame_time = int(user.ingame_time)
+			self.out_SERVERMSG(client, "Agent: %s" % (user.last_agent))
+			self.out_SERVERMSG(client, "Registered %s" % register_date)
+			self.out_SERVERMSG(client, "Last login %s" % last_login)
 			self.out_SERVERMSG(client, "access=%s,  bot=%s,  ingame_time=%d hours" % (user.access, user.bot, ingame_time/60))
 			self.out_SERVERMSG(client, "email=%s" % (user.email))
 			self.out_SERVERMSG(client, "last_ip=%s" % (user.last_ip))
